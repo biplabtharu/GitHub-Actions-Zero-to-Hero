2ca4db4217549940efd80592eda046983402eae4
Biplab Tharu <kktbiplab010@gmail.com>
2025-02-27 20:49:30 +0545

    change parameters

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,10 @@
 # app.py
 # This is a test commit
-def add(c, d):
-    return c + d
+def add(e, f):
+    return e + f
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
     assert add(5,6) == 11
+    assert add(10,11) == 21
